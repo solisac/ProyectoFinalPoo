f31300fc6c3e7963a386dcbb6ad08d18b4c1ce3f
solisac <sys.solis@gmail.com>
2023-07-20 21:08:43 -0500

    Esta es la version V.1.0 del proyecto Final

---
+++ src/ec/edu/intsuperior/controlador/Controlador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.controlador;
+
+import ec.edu.intsuperior.vista.Inicio;
+import ec.edu.intsuperior.vista.Loggin;
+
+/**
+ *
+ * @author Santy
+ */
+public class Controlador {
+    
+    Loggin loggin= new Loggin(this);
+    Inicio inicio= new Inicio(this);
+    
+    public void mostrarInicio(){
+        inicio.setVisible(true);
+    }
+    
+    public void mostrarLoggin(){
+        loggin.setVisible(true);
+    }
+    
+}
